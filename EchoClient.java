3e6237242a39e39ef6d7f9d47b13fc6c80f251ce
pedersph <ppedersen87@gmail.com>
2018-10-07 17:58:46 -0400

    Add files via upload

---
+++ EchoClient.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+public class EchoClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean open = true;
+		try {
+			
+			Socket sender = new Socket("localhost",8008);
+			if(sender.isConnected()) {
+				System.out.println("Connected");
+			}
+			Scanner userInput = new Scanner(System.in);
+			BufferedReader message = new BufferedReader(new InputStreamReader(sender.getInputStream()));
+			PrintWriter out = new PrintWriter(new OutputStreamWriter(sender.getOutputStream()));
+			
+			while(!userInput.hasNextLine()) {
+				String str = userInput.nextLine();
+				out.println(str);
+				System.out.println(message.readLine());
+			}
+			message.close();
+			out.close();
+			sender.close();
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
